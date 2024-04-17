f8d09c77ba79516c4129a40ac854ed9e0dd5fe9c
Ali.babaeii <alii.babaei1990@gmail.com>
2024-04-17 21:36:08 +0200

    Add cart store mobx

---
+++ src/app/mobx/CartStore.ts
@@ -0,0 +1,45 @@
+import { action, makeObservable, observable } from 'mobx'
+import HistoryStore from './HistoryStore'
+
+interface CartItem {
+	code: string
+	quantity: number
+	id: number
+}
+
+class CartStore {
+	items: CartItem[] = []
+
+	constructor() {
+		makeObservable(this, {
+			items: observable,
+			addItem: action,
+			removeItem: action,
+			moveToHistory: action
+		})
+	}
+
+	addItem(item, category) {
+		const existingItem = this.items.find((cartItem) => cartItem.code === item.code)
+
+		if (existingItem) {
+			// If item with the same code already exists in the cart, increment its quantity
+			existingItem.quantity++
+		} else {
+			// If item doesn't exist in the cart, add it with the specified category
+			this.items.push({ ...item, quantity: 1, category })
+		}
+	}
+
+	removeItem(index) {
+		this.items.splice(index, 1)
+	}
+
+	moveToHistory() {
+		HistoryStore.addItems(this.items)
+		this.items = []
+	}
+}
+
+const cartStore = new CartStore()
+export default cartStore
